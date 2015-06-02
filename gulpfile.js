8ae631c31980a18ead9e54675d157f0f1b05c790
Glenn Dixon <glenn@squareup.com>
2015-06-02 14:36:37 -0700

    templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var bourbon = require('node-bourbon').includePaths;
 var runSequence = require('run-sequence');
 var del = require('del');
 var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
+var concat = require('gulp-concat');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var templateCache = require('gulp-angular-templatecache');
 var isProduction = false;
 
-gulp.task("_scripts", function() {
-  return gulp.src('js/main.js')
-    .pipe(browserify({
-      debug: !isProduction,
-      transform: ["babelify"]
-    }))
+gulp.task("_templates", function(){
+  return gulp.src('templates/**/*.html')
+    .pipe(templateCache({module:'helloApp'}))
+    .pipe(rename('templates.bundle.js'))
+    .pipe(gulp.dest('tmp'))
+});
+
+gulp.task("_app_scripts", function() {
+  return browserify({
+      entries: 'js/main.js',
+      debug: true,
+      transform: [babelify]
+    })
+    .bundle()
+    .pipe(source('app.bundle.js'))
+    .pipe(buffer())
     .pipe(uglify())
-    .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('tmp'));
+});
+
+gulp.task('_scripts', ['_templates', '_app_scripts'], function() {
+  return gulp.src(['tmp/app.bundle.js', 'tmp/templates.bundle.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('_styles', function() {
--- js/controllers/hello.js
+++ js/controllers/hello.js
@@ -1,5 +1,19 @@
 import app from '../app';
 
-app.controller("HelloController", ['$scope', function($scope) {
+app.controller("HelloController", ['$scope', '$templateCache', function($scope, $templateCache) {
 	$scope.name = "Calvin Hobbes";
+
+  window.blah = $templateCache;
+	console.log($templateCache.get('template1.html'));
+	$scope.templateName = 'template1.html';
 }]);
+
+// angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("template1.html","FOOOOOO\n");
+// $templateCache.put("foo/template1.html","FOOOOOO\n");}]);
+
+//
+// app.run(['$templateCache',
+//   function($templateCache) {
+//     $templateCache.put("template1.html", "DERP DERP");
+//   }
+// ]);
