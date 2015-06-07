5f79cbebd5b96d24233258492f9ea9588c8afaf9
Glenn Dixon <glenndixon@gmail.com>
2015-06-06 17:30:57 -0700

    derp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,13 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var bourbon = require('node-bourbon').includePaths;
 var runSequence = require('run-sequence');
 var del = require('del');
-var isProduction = false;
-
-gulp.task("_scripts", function() {
-  return gulp.src('js/main.js')
-    .pipe(browserify({
-      debug: !isProduction,
-      transform: ["babelify"]
-    }))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('dist'));
-});
+var webserver = require('gulp-webserver');
 
 gulp.task('_styles', function() {
-  return gulp.src('css/main.sass')
+  return gulp.src('css/main.scss')
     .pipe(sass({
       includePaths: bourbon
     }))
@@ -27,13 +16,29 @@ gulp.task('_styles', function() {
 });
 
 gulp.task('_clean', function() {
-  return del(['dist/*']);
+  return del(['dist/*.css']);
+});
+
+gulp.task('_public', function(){ 
+  return gulp.src('public/**')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function(callback) {
+  runSequence('_clean', ['_styles', '_public'], function(){ 
+    callback();
+  });
+});
+
+gulp.task('serve', function() {
+  return gulp.src('dist')
+    .pipe(webserver());
 });
 
 gulp.task('watch', function(callback) {
-  return runSequence('_clean', ['_scripts', '_styles'], function() {
-    gulp.watch('js/**/*.js', ['_scripts']);
+  runSequence('build', 'serve', function() {
     gulp.watch('css/**/*.sass', ['_styles']);
+    gulp.watch('public/**', ['_public']);
     callback();
   });
 });
